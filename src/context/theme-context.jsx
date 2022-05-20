d2a0cb9284cb9579b18582574d2f8e5e8a019b26
rahulmistry751 <rahulmistry751@gmail.com>
2022-05-21 02:07:06 +0530

    feat:add theme state to localStorage

--- src/context/theme-context.jsx
+++ src/context/theme-context.jsx
@@ -1,9 +1,11 @@
-import { createContext,useContext, useState } from "react";
+import { createContext,useContext, useEffect, useState } from "react";
 const ThemeContext=createContext("light");
 
 const ThemeContextProvider=({children})=>{
-    const [theme,setTheme]=useState("light");
-
+    const [theme,setTheme]=useState(localStorage.getItem("colorTheme")??"light");
+    useEffect(()=>{
+       localStorage.setItem("colorTheme",theme);
+    },[theme])
     return(
         <ThemeContext.Provider value={{theme,setTheme}}>
             {children}
